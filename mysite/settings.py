bbbf6241dd064204f6ef6a6fd3718a92707ed2a8
rajatrc1705 <rajatrc1705@gmail.com>
2021-05-01 12:14:40 +0530

    removed faulty settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
 
-env = environ.Env()
-environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY='django-insecure-$3vrv5tktm=()tuc%@(&i&9s4kt$%=-r$&sh(xq$643+^f*_6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
