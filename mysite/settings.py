5dcb09773f3c0ba31e1c58dd56dc5d6baade3125
rajatrc1705 <rajatrc1705@gmail.com>
2021-05-01 11:59:37 +0530

    changed some settings related to secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3vrv5tktm=()tuc%@(&i&9s4kt$%=-r$&sh(xq$643+^f*_6r'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
